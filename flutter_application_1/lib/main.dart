4f68f41e3b0a72f2abf472ecc7ad551ed09d790a
Shishir islam shanto <mdsabirulislam8089@gmail.com>
2023-03-28 17:39:44 +0600

    create login form

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/Homepage.dart';
 import 'pages/login_page.dart';
 
 void main() {
-  runApp(const Myapp());
+  runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
-  const Myapp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: LoginPage(),
+      debugShowCheckedModeBanner: false,
       initialRoute: "/home",
+      theme: ThemeData(
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       routes: {"/": (context) => LoginPage(), "/home": (context) => HomePage()},
     );
   }
